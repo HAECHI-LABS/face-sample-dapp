7776015b03ed6a0476f80143daa980d8654e40df
Taejung Kim <immcoc1@gmail.com>
2023-08-18 13:26:49 +0900

    fix: polyfill nodejs Buffer

--- src/index.js
+++ src/index.js
@@ -1,14 +1,11 @@
 import 'bulma/css/bulma.css';
 import './index.css';
 
-import { Buffer } from 'buffer';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { RecoilRoot } from 'recoil';
 
 import App from './App';
-
-global.Buffer = Buffer;
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
